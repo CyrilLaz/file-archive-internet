4643985d569fee0377daa062c42186a6c4ef1602
Cyril Lazar <klazar@mail.ru>
2023-01-14 01:09:30 +1000

    feat: при ошибке повторно скачивает файл, если много раз то скачивание прерывается

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const path = require('path');
 const appendToArhive = require('./controllers/appendToArhive');
 const readAndSend = require('./controllers/readAndSend');
 
-const {
-  URL="https://www.dns-shop.ru",
-  SERVER,
-} = process.env;
-
+const { URL = 'https://www.dns-shop.ru', SERVER } = process.env;
 
 // console.log(process.argv);
 const SWITCH = Number(process.argv[2]);
@@ -17,23 +13,31 @@ const nameArchive = `archive.zip`;
 
 switch (SWITCH) {
   case 0:
-    appendToArhive(nameArchive, URL).then((archive) => {
-      console.log('#########');
-      console.log('Архив готов к отправке');
-      console.log(`Его размер ${archive.pointer()}`);
-    });
+    appendToArhive(nameArchive, URL)
+      .then((archive) => {
+        console.log('#########');
+        console.log('Архив готов к отправке');
+        console.log(`Его размер ${archive.pointer()}`);
+      })
+      .catch((err) => console.log(err));
     break;
 
   case 1:
-      readAndSend(nameArchive,SERVER).then((body) => {
-        const stats = fs.statSync(path.join(path.resolve('./temp'), nameArchive));
+    readAndSend(nameArchive, SERVER)
+      .then((body) => {
+        const stats = fs.statSync(
+          path.join(path.resolve('./temp'), nameArchive)
+        );
         const fileSizeInBytes = stats.size;
-        const res=JSON.parse(body);
+        const res = JSON.parse(body);
 
-        if (fileSizeInBytes===res.stats.size) {
+        if (fileSizeInBytes === res.stats.size) {
           console.log('Файл успешно отправлен');
-        } else{console.log('Что то пошло не так при отправке файла');}
-      });
+        } else {
+          console.log('Что то пошло не так при отправке файла');
+        }
+      })
+      .catch((err) => console.log(err));
 
     break;
 
--- controllers/appendToArhive.js
+++ controllers/appendToArhive.js
@@ -1,27 +1,40 @@
 const archive = require('../components/archivator');
 const fs = require('fs');
-const {resolve, join} = require('path');
+const { resolve, join } = require('path');
 const { getPage } = require('../components/getFiles');
-const dir = './temp'
+const dir = './temp';
 
-async function appendToArhive(nameArchive,URL) {
+async function appendToArhive(nameArchive, URL) {
   if (!fs.existsSync(dir)) {
     // проверка наличия папки архива
     fs.mkdirSync(dir);
   }
   const output = fs.createWriteStream(join(resolve('./temp'), nameArchive)); //path.join(path.resolve('./temp'), files[0])
+  let count=0;
   archive.pipe(output);
   console.log('Начали скачивание');
   for (let index = 1; index <= 14; index++) {
     const name = `ymrf_${index}.html`;
     const url = `${URL}/${name}`;
-    await getPage(url).then((page) => archive.append(page, { name })).catch(err=>console.log(err));
+    if(count>3){
+     // иначе есть опастность словить петлю
+      return Promise.reject('Прерываем скачивание иначе долго и зациклились, попробуй скачать еще раз');
+    } 
+    await getPage(url)
+      .then((page) => archive.append(page, { name }))
+      .catch((err) => {
+        index=index-1;
+        count++;
+        console.log('Попробуем скачать еще раз, а то ошибка');
+      });
+
+
     console.log(`${name} готов, пошли дальше`);
   }
 
   await archive.finalize();
 
-  return archive
+  return archive;
 }
 
 module.exports = appendToArhive;
